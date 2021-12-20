911e44f4812be32fc5db131eac43406997dca93c
김병훈 <figma@kakao.com>
2021-12-20 15:27:25 +0900

    TS: FormEvent와 타입 연결하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import Circle from "./Circle"
+import React, { useState } from "react"
 
 function App() {
+  const [userName, setUserName] = useState("")
+
+  // React FormEvent
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event
+    setUserName(value)
+    // TODO: currentTarget과 target의 차이는?
+  }
+
+  // React FormEvent
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(userName)
+  }
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="tomato" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={userName}
+          onChange={onChange}
+          type="text"
+          placeholder="이름"
+        />
+        <button>로그인</button>
+      </form>
     </div>
   )
 }
